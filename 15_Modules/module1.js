23ce23014a7b0140ba049ebfe2d059ef129690e0
Aniket Meshram <aniketmeshramofficial@gmail.com>
2023-07-31 16:36:11 +0530

    initial commit

---
+++ 15_Modules/module1.js
@@ -0,0 +1,14 @@
+import list, {ans, name, collegeName} from "./module2.js";
+ans();
+
+console.log(`My college name is ${collegeName}`);
+
+name("aniket");
+name("aman");
+name("nayan");
+name("mehul");
+name("rocky");
+
+console.log(list[1].name);
+
+
---
+++ 15_Modules/module2.js
@@ -0,0 +1,29 @@
+export const collegeName = "Xavier Institute of Engineering, Mumbai"
+
+export const ans = () => {
+    console.log("hello");
+}
+
+
+export const name = (name) => {
+    console.log("hello " + name);
+}
+
+
+const list = [
+    {"name":"aniket", "roll": 35},
+    {"name":"aman", "roll": 36},
+    {"name":"nayan", "roll": 37},
+    {"name":"mehul", "roll": 38},
+];
+
+
+const fun = (a, b) => {
+    return (a + b);
+}
+
+
+
+//  a module cannot hava multiple default
+export default list;
+// export default fun
